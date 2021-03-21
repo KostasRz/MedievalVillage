490349a6b4c541ea0403d6974a6dba761e7ef837
Κώστας Ρίζος <rizakos99@gmail.com>
2021-03-22 00:57:40 +0200

    npc

---
+++ MedievalVillage/Assets/Kevin Iglesias/Basic Motions Pack/AnimationControllers/CharacterDestination.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDestination : MonoBehaviour
+{
+
+    public int xPos;
+    public int zPos;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            xPos = Random.Range(332, 385);
+            zPos = Random.Range(514, 522);
+            this.gameObject.transform.position = new Vector3(xPos, 60.53f, zPos);
+        }
+
+    }
+}
