15ee857970803d6657c4272f66c745f8ce0bc6e0
Κώστας Ρίζος <rizakos99@gmail.com>
2021-03-29 20:14:17 +0300

    anoigokleinw portes kai klebw spitia mazi me neaterntal nano

---
+++ MedievalVillage/Assets/Scripts/InteractScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour
+{
+    public float interactDistance = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray(transform.position,transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, interactDistance))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.transform.GetComponent<DoorScript>().ChangeDoorState();
+                }
+            }
+
+        }
+    }
+}
+    
