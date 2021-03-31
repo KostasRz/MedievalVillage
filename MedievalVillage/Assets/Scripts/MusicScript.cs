c820096597e2f968d1226cbbfcd2119091c478e3
Κώστας Ρίζος <rizakos99@gmail.com>
2021-03-31 12:01:54 +0300

    mousikh apo rapsodo karampo

---
+++ MedievalVillage/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    public static MusicScript MusicInstance;
+    private void Awake()
+    {
+        if(MusicInstance!=null && MusicInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        MusicInstance = this;
+        DontDestroyOnLoad(this);
+    }
+}
