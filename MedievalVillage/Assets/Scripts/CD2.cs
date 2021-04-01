6d5b14d8b3718c1d321af70b213be1a79c7d2680
Κώστας Ρίζος <rizakos99@gmail.com>
2021-04-01 20:26:51 +0300

    npc fix

--- MedievalVillage/Assets/Scripts/CD2.cs
+++ MedievalVillage/Assets/Scripts/CD2.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class CD2 : MonoBehaviour
 {
-    public int xPos;
-    public int zPos;
+    public int xPos1;
+    public int zPos1;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Character2")
         {
-            xPos = Random.Range(335, 379);
-            zPos = Random.Range(473, 479);
-            this.gameObject.transform.position = new Vector3(xPos, 60.5f, zPos);
+            xPos1 = Random.Range(335, 373);
+            zPos1 = Random.Range(473, 479);
+            this.gameObject.transform.position = new Vector3(xPos1, 60.5f, zPos1);
         }
 
     }
